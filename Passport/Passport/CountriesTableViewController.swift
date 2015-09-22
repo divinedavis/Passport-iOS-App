59866b039e48c5e622cb635f58092181813381c6
divinedavis <divinejdavis@gmail.com>
2015-09-22 11:16:10 -0400

    Completed first portion of my passport app. - Added an image - Added a button that pushes to a table view - Added 6 labels - Added navigation controller

---
+++ Passport/Passport/CountriesTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  CountriesTableViewController.swift
+//  Passport
+//
+//  Created by Divine Davis on 9/22/15.
+//  Copyright © 2015 Juniper Street. All rights reserved.
+//
+
+import UIKit
+
+class CountriesTableViewController: UITableViewController {
+
+    
+    var countries = ["Italy", "Norway", "England"]
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as UITableViewCell
+        let country = countries[indexPath.row]
+        cell.textLabel?.text = country
+        return cell
+    }
+    
+    
+}

e6b9bf9c2b69010bb618f28b4436a49deb540929
divinedavis <divinejdavis@gmail.com>
2015-09-25 22:11:31 -0400

    Adding annotations to the map - Adding an array to the map - Annotations will populate the map - A small description will pop up on top of the annotation

--- Passport/Passport/MapViewController.swift
+++ Passport/Passport/MapViewController.swift
@@ -7,7 +7,42 @@
 //
 
 import UIKit
+import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet weak var myMapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let italy = MKPointAnnotation()
+        italy.coordinate = CLLocationCoordinate2D(latitude: 41.8947400, longitude: 12.4839000)
+        italy.title = "Rome, Italy"
+        
+        let england = MKPointAnnotation()
+        england.coordinate = CLLocationCoordinate2D(latitude: 51.5085300, longitude: -0.1257400)
+        england.title = "London, England"
+        
+        let norway = MKPointAnnotation()
+        norway.coordinate = CLLocationCoordinate2D(latitude: 59.914225, longitude: 10.75256)
+        norway.title = "Oslo, Norway"
+        
+        let spain = MKPointAnnotation()
+        spain.coordinate = CLLocationCoordinate2D(latitude: 40.41694, longitude: -3.70081)
+        spain.title = "Madrid, Spain"
+        
+        let locations = [italy, england, norway, spain]
+        
+        myMapView.addAnnotation(locations)
+        
+    }
+    
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        let pin = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "pinIdentifier")
+        pin.canShowCallout = true
+        return pin
+        
+    }
 
 }

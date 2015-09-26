e5ffb5b4ba56e52545026e0c62993077c6377d5a
divinedavis <divinejdavis@gmail.com>
2015-09-25 20:48:04 -0400

    Adding frameworks for MapKit. Going to learn how to track the location with the map.

---
+++ Passport/Passport/MapViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MapViewController.swift
+//  Passport
+//
+//  Created by Divine Davis on 9/25/15.
+//  Copyright © 2015 Juniper Street. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+}
